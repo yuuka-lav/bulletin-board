8487cae3a44dbad34b27476851e83245f1710852
小高優香 <kotaka.yuuka.infratop@gmail.com>
2020-11-03 21:35:11 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム: </label>
+    <input id="name" type="text" v-model="name">
+    <br><br>
+    <label for="comment">コメント: </label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <h2>掲示板</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: '',
+      comment: '',
+    }
   }
 }
 </script>
